c637d1228366084a46a1bf7187100cd128e7517b
unknown <m.daffa342@gmail.com>
2024-10-22 15:41:47 +0700

    Latihan Deret Bilangan

---
+++ Latihan/deretbilangan.py
@@ -0,0 +1,14 @@
+#dimulai dari index 0
+n = int(input('Banyak Data : '))
+for i in range(n):
+    print(i)
+
+#dimulai dari angka 2
+n = int(input('Banyak Data : '))
+for i in range(2,n):
+    print(i)
+
+#dimulai dari angka 2 dan kelipatan 2
+n = int(input('Banyak Data : '))
+for i in range(2,n,2):
+    print(i)
